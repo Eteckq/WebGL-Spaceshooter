82bf50a3abef84d8569b40d863057348aac7c6f3
Yohan <yohan.g99@gmail.com>
2021-03-10 15:24:45 +0100

    Update fast-enemy-missile.ts

--- src/js/objects/projectiles/enemy/fast-enemy-missile.ts
+++ src/js/objects/projectiles/enemy/fast-enemy-missile.ts
@@ -2,17 +2,31 @@ import GameManager from '../../../game-manager'
 import EnemyMissile from '../../abstract/enemy-missile'
 
 export default class FastEnemyMissile extends EnemyMissile {
+  d: number
+  speedCoef: number
+  xCoef: number
+  yCoef: number
   constructor() {
     super('laserBlue09', 0.03, 0.03)
     // TODO Get player position and aim at it
 
-    this.speed = 0.7
+    this.speed = 2
     let playerPosition = GameManager.Instance.playerManager.player.getPosition()
-    console.log(playerPosition)
+    
+    
+    this.d = Math.sqrt(
+      Math.pow(playerPosition.x - this.getPosition().x, 2) +
+        Math.pow(playerPosition.y - this.getPosition().y, 2)
+    )
+
+    this.speedCoef = this.speed / this.d
+
+    this.xCoef = (playerPosition.x - this.getPosition().x) * 0.005
+    this.yCoef = (playerPosition.y - this.getPosition().y) * 0.005
   }
 
   public update() {
-    this.position[1] += this.speed * -0.05
-    this.position[0] += this.speed * 0.0007
+    this.position[0] += this.xCoef * this.speedCoef
+    this.position[1] += this.yCoef * this.speedCoef
   }
 }
