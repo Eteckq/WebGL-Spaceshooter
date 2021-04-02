4b0bdf0a78e1b86a751bf560265215ab75a7f231
Yohan <yohan.g99@gmail.com>
2021-04-02 13:46:27 +0200

    fix hp bar on restart

--- src/js/objects/weapons/basic-weapon.ts
+++ src/js/objects/weapons/basic-weapon.ts
@@ -1,65 +1,56 @@
-import { Vector3 } from '../../../../node_modules/@math.gl/core/src/index'
-import Weapon from '../abstract/weapon'
-import BasicMissile from '../projectiles/player/basic-missile'
+import { Vector3 } from "../../../../node_modules/@math.gl/core/src/index";
+import Weapon from "../abstract/weapon";
+import BasicMissile from "../projectiles/player/basic-missile";
 
 export default class BasicWeapon extends Weapon {
   constructor() {
-    super()
-    this.SHOOT_COOLDOWN = 25
+    super();
+    this.SHOOT_COOLDOWN = 25;
   }
 
   public use(position: Vector3, level: number) {
     if (level >= 5) {
-      this.shootCooldown -= 4 * (level - 4)
+      this.shootCooldown -= 4 * (level - 4);
     }
     switch (level) {
       case 1:
-        this.shootLevel1(position)
-        break
+        this.shootLevel1(position);
+        break;
       case 2:
-        this.shootLevel2(position)
-        break
+        this.shootLevel2(position);
+        break;
       case 3:
-        this.shootLevel3(position)
-        break
+        this.shootLevel3(position);
+        break;
       default:
-        this.shootLevel4(position)
-        break
+        this.shootLevel4(position);
+        break;
     }
   }
 
   private shootLevel1(position: Vector3) {
-    new BasicMissile(position)
+    new BasicMissile(position);
   }
   private shootLevel2(position: Vector3) {
-    new BasicMissile(new Vector3(position.x - 0.02, position.y, 0.5))
-    new BasicMissile(new Vector3(position.x + 0.02, position.y, 0.5))
+    new BasicMissile(new Vector3(position.x - 0.02, position.y, 0.5));
+    new BasicMissile(new Vector3(position.x + 0.02, position.y, 0.5));
   }
   private shootLevel3(position: Vector3) {
-    this.shootLevel2(position)
+    this.shootLevel2(position);
 
-    new BasicMissile(new Vector3(position.x, position.y - 0.05, 0.5), 10)
-    new BasicMissile(new Vector3(position.x, position.y - 0.05, 0.5), -10)
+    new BasicMissile(new Vector3(position.x, position.y - 0.05, 0.5), 10);
+    new BasicMissile(new Vector3(position.x, position.y - 0.05, 0.5), -10);
   }
   private shootLevel4(position: Vector3) {
-    let bm6 = new BasicMissile(
-      new Vector3(position.x, position.y - 0.07, 0.5),
-      15
-    )
-    let bm5 = new BasicMissile(
+    new BasicMissile(new Vector3(position.x, position.y - 0.07, 0.5), 15);
+    new BasicMissile(
       new Vector3(position.x + 0.02, position.y - 0.07, 0.5),
       -15
-    )
-    let bm3 = new BasicMissile(
-      new Vector3(position.x, position.y - 0.05, 0.5),
-      10
-    )
-    let bm4 = new BasicMissile(
-      new Vector3(position.x, position.y - 0.05, 0.5),
-      -10
-    )
-    let bm0 = new BasicMissile(new Vector3(position.x, position.y, 0.5))
-    let bm1 = new BasicMissile(new Vector3(position.x + 0.04, position.y, 0.5))
-    let bm2 = new BasicMissile(new Vector3(position.x - 0.04, position.y, 0.5))
+    );
+    new BasicMissile(new Vector3(position.x, position.y - 0.05, 0.5), 10);
+    new BasicMissile(new Vector3(position.x, position.y - 0.05, 0.5), -10);
+    new BasicMissile(new Vector3(position.x, position.y, 0.5));
+    new BasicMissile(new Vector3(position.x + 0.04, position.y, 0.5));
+    new BasicMissile(new Vector3(position.x - 0.04, position.y, 0.5));
   }
 }
--- src/js/view.ts
+++ src/js/view.ts
@@ -1,45 +1,48 @@
-import * as $ from 'jquery'
-import { startGame } from '../app'
-import BasicEnemy from './objects/enemies/basic'
-import FastEnemy from './objects/enemies/fast'
-import TankEnemy from './objects/enemies/tank'
-import UfoEnemy from './objects/enemies/ufo'
-import StrongEnemy from './objects/enemies/strong'
+import * as $ from "jquery";
+import { startGame } from "../app";
+import BasicEnemy from "./objects/enemies/basic";
+import FastEnemy from "./objects/enemies/fast";
+import TankEnemy from "./objects/enemies/tank";
+import UfoEnemy from "./objects/enemies/ufo";
+import StrongEnemy from "./objects/enemies/strong";
 
-const api = 'https://api-score.yohangastoud.fr/scores'
+const api = "https://api-score.yohangastoud.fr/scores";
 class View {
-  private healtBar
-  public pseudo: string = ''
+  private healtBar;
+  public pseudo: string = "";
 
   constructor() {
-    this.healtBar = new HealthBar()
-    this.setScore(0)
-    this.setWaves(0)
+    this.healtBar = new HealthBar();
+    this.setScore(0);
+    this.setWaves(0);
   }
 
   public setHp(hp: number) {
-    this.healtBar.setHp(hp)
+    if (hp < 0) {
+      hp = 0;
+    }
+    this.healtBar.setHp(hp);
   }
 
   public setInvul(invul: boolean) {
     if (invul) {
-      $('.bar').css('background-color', '#4169E1')
+      $(".bar").css("background-color", "#4169E1");
     } else {
-      $('.bar').css('background-color', '#c54')
+      $(".bar").css("background-color", "#c54");
     }
   }
 
   // TODO Use UI icon to display score
   public setScore(score: number) {
-    $('.info_SCORE').html(score.toString())
+    $(".info_SCORE").html(score.toString());
   }
 
   public setWaves(wave: number) {
-    $('.info_WAVES').html(wave.toString())
+    $(".info_WAVES").html(wave.toString());
   }
 
   public sendScore(score: number) {
-    $('#endgameModal').show()
+    $("#endgameModal").show();
 
     $.post(api, {
       payload: btoa(
@@ -51,36 +54,36 @@ class View {
         )
       ),
     }).then(() => {
-      this.setLeaderboard()
-    })
+      this.setLeaderboard();
+    });
   }
 
   public getLeaderboard() {
-    return $.get(api)
+    return $.get(api);
   }
 
   public setLeaderboard() {
-    let leaderboard = $('#LeaderboardList')
-    leaderboard.html('')
+    let leaderboard = $("#LeaderboardList");
+    leaderboard.html("");
 
     this.getLeaderboard().then((data) => {
       for (let i = 0; i < data.length; i++) {
-        const score = data[i]
+        const score = data[i];
         leaderboard.append(`
           <tr>
             <td class="position">#${i + 1}</td>
-            <td class="pseudo">${score.pseudo} ${i === 0 ? '👑' : ''}</td>
+            <td class="pseudo">${score.pseudo} ${i === 0 ? "👑" : ""}</td>
             <td class="score">${score.score}</td>
           </tr>
-        `)
+        `);
       }
-    })
+    });
   }
 
   public setBonus(list: string[]) {
-    $('#bonusList').html('')
+    $("#bonusList").html("");
     for (const bonus of list) {
-      $('#bonusList').append(`
+      $("#bonusList").append(`
         <li>
           <img
             src="${bonus}"
@@ -88,101 +91,101 @@ class View {
             width="30px"
           />
         </li>
-        `)
+        `);
     }
   }
 }
 
 class HealthBar {
-  hBar: any
-  bar: any
-  hit: any
-  val: any
+  hBar: any;
+  bar: any;
+  hit: any;
+  val: any;
 
   constructor() {
-    this.hBar = $('.health-bar')
-    this.bar = this.hBar.find('.bar')
-    this.hit = this.hBar.find('.hit')
-    this.val = this.hBar.find('.hpval')
+    this.hBar = $(".health-bar");
+    this.bar = this.hBar.find(".bar");
+    this.hit = this.hBar.find(".hit");
+    this.val = this.hBar.find(".hpval");
   }
 
   public setHp(hp: number) {
-    let total = this.hBar.data('total')
-    let value = this.hBar.data('value')
-    let diff = hp - value
+    let total = this.hBar.data("total");
+    let value = this.hBar.data("value");
+    let diff = hp - value;
 
-    this.val.html(hp)
+    this.val.html(hp);
     if (value < 0) {
-      return
+      return;
     }
 
-    var barWidth = (hp / total) * 100
-    var hitWidth = (hp / value) * 100 + '%'
+    var barWidth = (hp / total) * 100;
+    var hitWidth = (hp / value) * 100 + "%";
 
-    this.hit.css('width', hitWidth)
-    this.hBar.data('value', hp)
+    this.hit.css("width", hitWidth);
+    this.hBar.data("value", hp);
 
     if (diff < 0) {
       setTimeout(() => {
-        this.hit.css({ width: '0' })
-        this.bar.css('width', barWidth + '%')
-      }, 500)
+        this.hit.css({ width: "0" });
+        this.bar.css("width", barWidth + "%");
+      }, 500);
     } else {
-      this.hit.css({ width: '0' })
-      this.bar.css('width', barWidth + '%')
+      this.hit.css({ width: "0" });
+      this.bar.css("width", barWidth + "%");
     }
   }
 
   public fullBar() {
-    this.hBar.data('value', this.hBar.data('total'))
+    this.hBar.data("value", this.hBar.data("total"));
 
-    this.hit.css({ width: '0' })
+    this.hit.css({ width: "0" });
 
-    this.bar.css('width', '100%')
+    this.bar.css("width", "100%");
   }
 }
 
-const view = new View()
+const view = new View();
 
 $(() => {
-  view.setLeaderboard()
+  view.setLeaderboard();
 
-  $('ul.tabs li').click(function () {
-    var tab_id = $(this).attr('data-tab')
+  $("ul.tabs li").click(function () {
+    var tab_id = $(this).attr("data-tab");
 
-    $('ul.tabs li').removeClass('current')
-    $('.tab-content').removeClass('current')
+    $("ul.tabs li").removeClass("current");
+    $(".tab-content").removeClass("current");
 
-    $(this).addClass('current')
-    $('#' + tab_id).addClass('current')
-  })
+    $(this).addClass("current");
+    $("#" + tab_id).addClass("current");
+  });
 
-  $('#restartBtn').on('click', () => {
-    startGame()
-    $('#endgameModal').hide()
-  })
+  $("#restartBtn").on("click", () => {
+    startGame();
+    $("#endgameModal").hide();
+  });
 
-  $('#spawn01').on('click', () => {
-    new BasicEnemy()
-  })
-  $('#spawn02').on('click', () => {
-    new FastEnemy()
-  })
-  $('#spawn03').on('click', () => {
-    new TankEnemy()
-  })
-  $('#spawn04').on('click', () => {
-    new UfoEnemy()
-  })
-  $('#spawn05').on('click', () => {
-    new StrongEnemy()
-  })
+  $("#spawn01").on("click", () => {
+    new BasicEnemy();
+  });
+  $("#spawn02").on("click", () => {
+    new FastEnemy();
+  });
+  $("#spawn03").on("click", () => {
+    new TankEnemy();
+  });
+  $("#spawn04").on("click", () => {
+    new UfoEnemy();
+  });
+  $("#spawn05").on("click", () => {
+    new StrongEnemy();
+  });
 
-  window.addEventListener('keydown', function (e) {
+  window.addEventListener("keydown", function (e) {
     if (e.keyCode == 32 && e.target == document.body) {
-      e.preventDefault()
+      e.preventDefault();
     }
-  })
-})
+  });
+});
 
-export default view
+export default view;
