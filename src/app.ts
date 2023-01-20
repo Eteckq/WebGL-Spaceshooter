6bd0f562e9415d7da8a18b4e689ea084230f0117
Yohan <yohan.gastoud@patrowl.io>
2023-01-20 14:42:45 +0100

    fix & obfuscate build

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ function tick () {
   gameManager.tick()
   setTimeout(() => {
     requestAnimationFrame(tick);
-  }, 1000 / 60);
+  }, 1000 / 144);
 }
 
 // eslint-disable-next-line import/prefer-default-export
